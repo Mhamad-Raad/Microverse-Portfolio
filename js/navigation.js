92b57b3b95ce92e32a6eb1a19856b9b255b12d00
joker69216 <hamaraad883@gmail.com>
2022-09-06 17:23:37 +0300

    fix linter errors

--- js/navigation.js
+++ js/navigation.js
@@ -1,45 +1,34 @@
-
-let burgerMenu = document.querySelector('.menu-burger-icon');
-let menu = document.querySelector('.mobile-menu');
-
+const burgerMenu = document.querySelector('.menu-burger-icon');
+const menu = document.querySelector('.mobile-menu');
 function openMenu() {
   burgerMenu.style.display = 'none';
   menu.style.display = 'flex';
 }
-
 burgerMenu.addEventListener('click', openMenu);
-
-
-let closeMenubtn = document.querySelector('.fa-xmark');
-
+const closeMenubtn = document.querySelector('.fa-xmark');
 function closeMenu() {
   burgerMenu.style.display = 'block';
     menu.style.display = 'none';
 }
-
 closeMenubtn.addEventListener('click', closeMenu);
-
-let mainMenu = document.querySelector('.main-menu');
-let aboutMyself = document.querySelector('.about-myself-menu');
-let contactMeMenu = document.querySelector('.contact-me-menu');
-let worksMenu = document.querySelector('.works-menu');
+const mainMenu = document.querySelector('.main-menu');
+const aboutMyself = document.querySelector('.about-myself-menu');
+const contactMeMenu = document.querySelector('.contact-me-menu');
+const worksMenu = document.querySelector('.works-menu');
 
 mainMenu.addEventListener('click', function() {
    closeMenu();
-   window.location = "#main";
+   window.location = '#main';
 });
-
 aboutMyself.addEventListener('click', function() {
   closeMenu();
-  window.location = "#about-myself";
+  window.location = '#about-myself';
 });
-
 worksMenu.addEventListener('click', function() {
   closeMenu();
-  window.location = "#works";
+  window.location = '#works';
 });
-
 contactMeMenu.addEventListener('click', function() {
   closeMenu();
-  window.location = "#contact-me";
+  window.location = '#contact-me';
 });
