9f04eb7ef08f89ee144aadf655276b679801d806
joker69216 <hamaraad883@gmail.com>
2022-09-06 16:40:47 +0300

    add navigation with js to menu

--- js/navigation.js
+++ js/navigation.js
@@ -2,9 +2,44 @@
 let burgerMenu = document.querySelector('.menu-burger-icon');
 let menu = document.querySelector('.mobile-menu');
 
+function openMenu() {
+  burgerMenu.style.display = 'none';
+  menu.style.display = 'flex';
+}
 
-burgerMenu.addEventListener('click', function() {
-    burgerMenu.style.display = 'none';
-    menu.style.display = 'flex';
-    console.log('clicked');
+burgerMenu.addEventListener('click', openMenu);
+
+
+let closeMenubtn = document.querySelector('.fa-xmark');
+
+function closeMenu() {
+  burgerMenu.style.display = 'block';
+    menu.style.display = 'none';
+}
+
+closeMenubtn.addEventListener('click', closeMenu);
+
+let mainMenu = document.querySelector('.main-menu');
+let aboutMyself = document.querySelector('.about-myself-menu');
+let contactMeMenu = document.querySelector('.contact-me-menu');
+let worksMenu = document.querySelector('.works-menu');
+
+mainMenu.addEventListener('click', function() {
+   closeMenu();
+   window.location = "#main";
+});
+
+aboutMyself.addEventListener('click', function() {
+  closeMenu();
+  window.location = "#about-myself";
+});
+
+worksMenu.addEventListener('click', function() {
+  closeMenu();
+  window.location = "#works";
+});
+
+contactMeMenu.addEventListener('click', function() {
+  closeMenu();
+  window.location = "#contact-me";
 });
