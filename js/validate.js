3c572299788ecedc6bd48bc011ec824f02b2ef38
joker69216 <hamaraad883@gmail.com>
2022-09-08 02:38:19 +0300

    fix js linter errors

--- js/validate.js
+++ js/validate.js
@@ -4,18 +4,18 @@ const msg = 'Please enter a valid email address (ALL in LOWER case)';
 
 form.addEventListener('submit', (e) => {
   if (form.elements.name.value !== '') {
-    console.log(form.elements.em.value);
     if (String(form.elements.em.value) !== String(form.elements.em.value).toLowerCase()) {
       errorMsg.innerHTML = msg;
       e.preventDefault();
-    }
-    else {
+      return false;
+    } else {
       errorMsg.innerHTML = '';
       return true;
     }
   } else {
     e.preventDefault();
     alert('Please enter your name');
+    return false;
   }
-    
+  return false;    
 });
