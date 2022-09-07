326998315fa041d6cb3c7b447cfb3ded9900de33
joker69216 <hamaraad883@gmail.com>
2022-09-08 01:43:05 +0300

    add functionality to error msgs

---
+++ js/validate.js
@@ -0,0 +1,22 @@
+const form = document.getElementById('form');
+const formbtn = document.querySelector('.contact-me-button');
+const errorMsg = document.querySelector('.error-msg');
+const msg = 'Please enter a valid email address (ALL in LOWER case)';
+
+form.addEventListener('submit', (e) => {
+  if (form.elements['name'].value !== '') {
+    console.log(form.elements['em'].value);
+    if(String(form.elements['em'].value) !== String(form.elements['em'].value).toLowerCase()) {
+      errorMsg.innerHTML = msg;
+      e.preventDefault();
+    }
+    else {
+      errorMsg.innerHTML = '';
+      return true;
+    }
+  } else {
+    e.preventDefault();
+    alert('Please enter your name');
+  }
+    
+});
