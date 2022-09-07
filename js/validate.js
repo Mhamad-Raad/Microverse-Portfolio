aefa4ce5e2497dc321575a29c3efcb5079930ad2
joker69216 <hamaraad883@gmail.com>
2022-09-08 02:33:56 +0300

    fix js linter errors

--- js/validate.js
+++ js/validate.js
@@ -1,12 +1,11 @@
 const form = document.getElementById('form');
-const formbtn = document.querySelector('.contact-me-button');
 const errorMsg = document.querySelector('.error-msg');
 const msg = 'Please enter a valid email address (ALL in LOWER case)';
 
 form.addEventListener('submit', (e) => {
-  if (form.elements['name'].value !== '') {
-    console.log(form.elements['em'].value);
-    if(String(form.elements['em'].value) !== String(form.elements['em'].value).toLowerCase()) {
+  if (form.elements.name.value !== '') {
+    console.log(form.elements.em.value);
+    if (String(form.elements.em.value) !== String(form.elements.em.value).toLowerCase()) {
       errorMsg.innerHTML = msg;
       e.preventDefault();
     }
